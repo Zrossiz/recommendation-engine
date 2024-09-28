e9c947afdedf926c59005754f7784cba4e995e95
Zrossiz <soon.pluxury@mail.ru>
2024-09-28 17:59:37 +0300

    feat: created methods for init dbs

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -1,1 +1,25 @@
 package postgresql
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type DBStorage struct {
+	db *pgxpool.Pool
+}
+
+func Connect(dsn string) (*pgxpool.Pool, error) {
+	db, err := pgxpool.Connect(context.Background(), dsn)
+	if err != nil {
+		return nil, fmt.Errorf("connect fail")
+	}
+
+	return db, nil
+}
+
+func New(dbConn *pgxpool.Pool) *DBStorage {
+	return &DBStorage{db: dbConn}
+}
--- internal/storage/redis/redis.go
+++ internal/storage/redis/redis.go
@@ -1,1 +1,21 @@
 package redis
+
+import "github.com/redis/go-redis/v9"
+
+type RedisStorage struct {
+	db *redis.Client
+}
+
+func Connect(addr string, password string, db int) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+
+	return rdb
+}
+
+func New(rdb *redis.Client) *RedisStorage {
+	return &RedisStorage{db: rdb}
+}

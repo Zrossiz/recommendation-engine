1db796c081c02e314045d966c9202a6c7f53366f
Zrossiz <soon.pluxury@mail.ru>
2024-10-01 15:51:37 +0300

    feat: created models for db

--- internal/model/models.go
+++ internal/model/models.go
@@ -1,1 +1,51 @@
-package models
+package model
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Category struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Content struct {
+	ID          int64     `json:"id"`
+	CategoryID  int64     `json:"category_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type Comment struct {
+	ID     int64  `json:"id"`
+	UserID int64  `json:"user_id"`
+	Text   string `json:"text"`
+}
+
+type RefreshToken struct {
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	Token     string    `json:"token"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserInteraction struct {
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	ContentID int64     `json:"content_id"`
+	Action    string    `json:"action"`    // like, dislike, view
+	ViewTime  int16     `json:"view_time"` // percent watched of viewed content
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}

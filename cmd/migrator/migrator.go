ba5bbca10820b265616b0032d74d3402e1217a6f
Zrossiz <soon.pluxury@mail.ru>
2024-10-02 10:30:11 +0300

    feat: added multiply migrate sql files

--- cmd/migrator/migrator.go
+++ cmd/migrator/migrator.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/joho/godotenv"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -20,18 +23,58 @@ func main() {
 	}
 	defer db.Close()
 
-	sqlFilePath := "migration/1_init.sql"
+	var filenameFlag string
+	flag.StringVar(&filenameFlag, "f", "", "filename for migrated file")
+	flag.Parse()
+
+	if filenameFlag == "" {
+		err = migrateAll(db)
+		if err != nil {
+			zap.S().Fatalf("error migrate all files: %v\n", err)
+		}
+	} else {
+		err = migrateOne(db, filenameFlag)
+		if err != nil {
+			zap.S().Fatalf("error migrate file: %v\n", err)
+		}
+	}
+
+	fmt.Println("Schema created successfully!")
+}
+
+func migrateAll(db *pgxpool.Pool) error {
+	files, err := os.ReadDir("migration")
+	if err != nil {
+		return err
+	}
+
+	for _, f := range files {
+		sqlFilePath := filepath.Join("migration", f.Name())
+		schemaSQL, err := os.ReadFile(sqlFilePath)
+		if err != nil {
+			return err
+		}
+
+		_, err = db.Exec(context.Background(), string(schemaSQL))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func migrateOne(db *pgxpool.Pool, filename string) error {
+	sqlFilePath := filepath.Join("migration", filename)
 	schemaSQL, err := os.ReadFile(sqlFilePath)
 	if err != nil {
-		fmt.Printf("error reading sql file: %v\n", err)
-		return
+		return err
 	}
 
 	_, err = db.Exec(context.Background(), string(schemaSQL))
 	if err != nil {
-		fmt.Printf("failed to execute schema SQL: %v\n", err)
-		return
+		return err
 	}
 
-	fmt.Println("Schema created successfully!")
+	return nil
 }
